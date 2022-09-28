250428dd92ea228bc3a5f66bc5dbe3fcba625115
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-09-29 03:54:17 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "02c85105-da34-441b-8fa9-5cd2256000f1",
+    "home_feed": [
+        {
+            "id": "7cc593f9-4e72-4d3f-a601-93e677c19960",
+            "artist_name": "common",
+            "home_carousel": [
+                {
+                    "id": "96ecc08c-dfab-4379-bf36-d282ebf39460",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/35.png"
+                },
+                {
+                    "id": "e0db742a-1cae-49db-861c-752b77e7c23b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/17.png"
+                },
+                {
+                    "id": "01f39b70-78c6-4de8-8217-e93a4f4fd04f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/302.png"
+                },
+                {
+                    "id": "b1e14b88-4ab3-416b-b632-6021e490d43c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/258.png"
+                },
+                {
+                    "id": "e12a512d-ee4e-4dab-bb7f-89e0fa2d7c6c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/350.png"
+                }
+            ],
+            "art_caption": "mention",
+            "art_description": "Social memory phone issue. Individual low deal. Life level gas these brother know detail everyone."
+        },
+        {
+            "id": "035c74dd-565f-43e0-bc49-c6a34b09fae5",
+            "artist_name": "maintain",
+            "home_carousel": [
+                {
+                    "id": "7a9d50ee-9abe-49f5-87e8-744f09850438",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/73.png"
+                },
+                {
+                    "id": "8a662d98-996d-487a-b02d-eb22bd67e00a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/70.png"
+                },
+                {
+                    "id": "90b46361-b594-44b4-8c64-2316515658fc",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/132.png"
+                },
+                {
+                    "id": "4e6e17ce-4430-4b82-9625-e29f8621a8ff",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/319.png"
+                },
+                {
+                    "id": "92386759-3384-46b6-a074-401b274c674a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/144.png"
+                }
+            ],
+            "art_caption": "range",
+            "art_description": "Interest group sign learn green along skill. Example understand cell. Federal American other south in political cut."
+        },
+        {
+            "id": "b860d7b5-4464-45db-b6f7-cb779b448240",
+            "artist_name": "the",
+            "home_carousel": [
+                {
+                    "id": "7d7b2012-0e28-4d0c-9257-3cafec9d39e4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/200.png"
+                },
+                {
+                    "id": "b6a73e47-4a64-47bb-92a0-58eeeeb77ee5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/26.png"
+                },
+                {
+                    "id": "09d74fd1-8fbf-4ae0-9cb3-fe98dad69cd3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/208.png"
+                },
+                {
+                    "id": "a8ea9c35-985f-4f6f-a788-291bd48086ad",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/19.png"
+                },
+                {
+                    "id": "a9dbfb24-8414-462a-91b4-98cbc52c9e33",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/104.png"
+                }
+            ],
+            "art_caption": "than",
+            "art_description": "Move yourself these against industry institution almost. Lawyer poor interview cold."
+        },
+        {
+            "id": "6bd57da2-1fa1-4127-b6ab-9054995e9aed",
+            "artist_name": "want",
+            "home_carousel": [
+                {
+                    "id": "936137ff-f32f-486d-9f32-64b6699f9f76",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/21.png"
+                },
+                {
+                    "id": "439caa6f-7446-4d2f-9a11-81b2ff392ed8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/251.png"
+                },
+                {
+                    "id": "fb206f24-35b3-4a69-bda5-9921c7ef9c08",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/366.png"
+                },
+                {
+                    "id": "cc66e771-9f34-4657-a826-100be3a13d3c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/44.png"
+                },
+                {
+                    "id": "63fc4c6c-107f-4f51-b4bd-ba6d295fd768",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/21.png"
+                }
+            ],
+            "art_caption": "shake",
+            "art_description": "Exactly quickly push require third part. Send arm however green draw item."
+        },
+        {
+            "id": "f7eaaeeb-c409-4b62-82f3-709dfd907463",
+            "artist_name": "or",
+            "home_carousel": [
+                {
+                    "id": "9964c506-7ed7-4bc8-871b-de09bc88bbaa",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/108.png"
+                },
+                {
+                    "id": "3a865bd4-486f-48d9-9875-5737a243b46e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/202.png"
+                },
+                {
+                    "id": "67012edc-f4e6-4a7c-a8a9-ceb5b5cc1692",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/227.png"
+                },
+                {
+                    "id": "6d16d7f5-79b4-4345-80fa-8ebcb4901ba0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/161.png"
+                },
+                {
+                    "id": "8e590a6b-6943-4425-a931-dc609bd9f933",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/179.png"
+                }
+            ],
+            "art_caption": "most",
+            "art_description": "Wife case five yourself just. Become include color good watch."
+        }
+    ]
+}
